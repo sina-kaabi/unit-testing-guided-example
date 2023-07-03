9d4f1022dbcd240388c5eff5421b09f6248e16bc
sina-kaabi <sina.kaabi@ordertiger.com>
2023-07-03 18:59:12 +0200

    solvedproblems

--- calculator.js
+++ calculator.js
@@ -1,13 +1,44 @@
 function sum(a, b) {
-  return;
+  if (a === undefined && b === undefined) {
+    return 0;
+  }
+
+  if (typeof a === 'number' && typeof b === 'undefined') {
+    return a + 0;
+  }
+
+  if (typeof a === 'undefined' && typeof b === 'number') {
+    return b + 0;
+  } else {
+    return a + b;
+  }
 }
 
 function subtract(a, b) {
-  return;
+  if (a === undefined && b === undefined) {
+    return 0;
+  }
+
+  if (typeof a === 'number' && typeof b === 'undefined') {
+    return a - 0;
+  }
+
+  if (typeof a === 'undefined' && typeof b === 'number') {
+    return b - 0;
+  } else {
+    return a - b;
+  }
 }
 
 function divide(a, b) {
-  return;
+  if (
+    (typeof a === 'number' && b === 0) ||
+    (a === 0 && typeof b === 'number')
+  ) {
+    throw new Error('An explanatory error message');
+  } else {
+    return a / b;
+  }
 }
 
 function multiply(a, b) {
